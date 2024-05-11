75040294767c1fa8be9bc918d7ebd8ff4e73bcc6
Gerson <pinedagerson1993@gmail.com>
2024-05-11 11:02:16 -0400

    JWT Middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ class UserController extends Controller
 
     public function __construct(UserRepositoryInterface $userRepository)
     {
+        $this->middleware('jwt.verify');
         $this->userRepository = $userRepository;
     }
 
